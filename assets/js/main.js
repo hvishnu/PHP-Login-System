45afe1784c89277d5737b39d9da94d4e2d7510d6
vishnu kant <hkantvishnu@gmail.com>
2019-04-04 22:55:53 +0530

    adding of ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,17 @@
-(document).on("submit",".js-register",function (event) {
+
+
+$(document)
+    .on("submit",".js-register",function (event) {
     event.preventDefault();
     var _form=$("this");
 
-    var _data={
+    var data={
         email: $("input [type='email']",_form).val(),
         password:$("input[type='password']",_form).val()
 
     }
     alert("form was submiiteerd");
-    console.log(_data);
+    console.log(data);
+    console.log("sonmtheignl")
+    return false;
 })
--- scripts.php
+++ scripts.php
@@ -1,2 +1,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="assets/js/main.js"></script>
